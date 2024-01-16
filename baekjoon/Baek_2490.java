78040b0c9f121413bcb08d8384a11113940b10f1
uzjaee <kyj960802@gmail.com>
2024-01-16 14:53:12 +0900

    Fix : Buffer사용을 통한 효율향상

--- baekjoon/Baek_2490.java
+++ baekjoon/Baek_2490.java
@@ -1,18 +1,24 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+
+
 public class Baek_2490{
-    public static void main(String [] args){
-        Scanner in = new Scanner(System.in);
+    public static void main(String [] args) throws IOException {
         String[] res = {"D","C","B","A","E"};
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         for (int j = 0; j<3; j++){
             int result = 0;
+            st = new StringTokenizer(bf.readLine());
             for (int i=0; i<4; i++){
-                 result += in.nextInt();
+                 result += Integer.parseInt(st.nextToken());
             }
-            System.out.println(res[result]);
+            bw.write(res[result] + "\n");
+            bw.flush();
         }
-
-
+        bw.close();
     }
 }
