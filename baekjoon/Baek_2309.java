f2bf9a0d5fd722a425dc439688a4c9d3925c6f23
uzjaee <kyj960802@gmail.com>
2024-02-08 16:01:30 +0900

    fix : 2309- 더 나은 알고리즘

--- baekjoon/Baek_2309.java
+++ baekjoon/Baek_2309.java
@@ -6,12 +6,32 @@ public class Baek_2309{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int [] arr = new int[9];
+        int [] index ;
+        int sum = 0;
+        int sub , first ,second;
+        first = 0;
+        second = 0;
         for(int i =0; i<arr.length;i++){
             arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
         Arrays.sort(arr);
-        for(int i =0; i<7;i++){
-            bw.write(String.valueOf(arr[i])+ "\n");
+        sub = sum - 100;
+        index = new int[sub];
+        for( int i = 0; i <arr.length; i++){
+            if(arr[i] >= sub)
+                continue;
+            if(index[sub - arr[i]] == 1){
+                first = sub- arr[i];
+                second = arr[i];
+            }
+            else
+                index[arr[i]] = 1;
+        }
+        for(int i = 0; i <arr.length; i++){
+            if(arr[i] != first && arr[i] != second ) {
+                bw.write(String.valueOf(arr[i]) + "\n");
+            }
         }
         bw.flush();
         bw.close();
