c69c78b321c9bb3cae68d1178019ab44545dbe59
uzjaee <kyj960802@gmail.com>
2024-02-26 11:24:44 +0900

    ADD : 15552 풀이 추가

---
+++ baekjoon/Baek_15552.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baek_15552 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for(int i =0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a+b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+
+
+    }
+}
