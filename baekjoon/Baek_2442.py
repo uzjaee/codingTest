f0a890e327bd465a3c8cc2cc4eeb44ffcb508f1c
uzjaee <kyj960802@gmail.com>
2024-02-29 17:23:34 +0900

    ADD 2442.py 풀이

---
+++ baekjoon/Baek_2442.py
@@ -0,0 +1,11 @@
+N = int(input())
+for i in range(1,N+1):
+    for j in range(N-i):
+        print(" " , end="")
+    for j in range(2*i-1):
+        print("*" , end="")
+    print("")
+
+
+    
+    
