6138d8a04a7a6fb6af3f3cee6fcf010265923e90
uzjaee <kyj960802@gmail.com>
2024-03-06 14:36:29 +0900

    ADD :10807 풀이

--- baekjoon/Baek_10807.java
+++ baekjoon/Baek_10807.java
@@ -1,2 +1,22 @@
-package baekjoon;public class Baek_10807 {
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Baek_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String [] st = br.readLine().split(" ");
+        int v = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(int i =0; i<st.length;i++){
+            if(v == Integer.parseInt(st[i]))
+                count++;
+        }
+        System.out.println(count);
+
+    }
 }
