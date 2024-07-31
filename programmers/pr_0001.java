65f4acc9dbc295ba95e4ff89b60f97548de5339c
uzjaee <kyj960802@gmail.com>
2024-07-31 09:27:49 +0900

    짝수 홀수 문제

---
+++ programmers/pr_0001.java
@@ -0,0 +1,10 @@
+package programmers;
+
+class pr_0001 {
+    public String solution(int num) {
+        String answer = "Odd";
+        if(num%2 == 0)
+            answer ="Even";
+        return answer;
+    }
+}
