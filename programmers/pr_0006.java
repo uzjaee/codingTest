870038cee054046adf36584e3f8524145e04c108
uzjaee <kyj960802@gmail.com>
2024-08-05 10:54:09 +0900

    x만큼 간격이 있는 n개의 숫자

--- programmers/pr_0006.java
+++ programmers/pr_0006.java
@@ -1,2 +1,15 @@
-package programmers;public class pr_0006 {
+package programmers;
+
+import java.util.ArrayList;
+
+public class pr_0006 {
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        long x2 = x;
+        for(int i=0; i<n;i++){
+            answer[i] = x;
+            x+=x2;
+        }
+        return answer;
+    }
 }
