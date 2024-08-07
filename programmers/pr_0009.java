a1ac3278b728028f2b0b5b4ce5d68dbf2dc26c4a
uzjaee <kyj960802@gmail.com>
2024-08-07 11:35:37 +0900

    정수 제곱근 판별 (정답풀이)

--- programmers/pr_0009.java
+++ programmers/pr_0009.java
@@ -1,27 +1,12 @@
 package programmers;
 
 public class pr_0009 {
-    static long check(long n,int k){
-        long answer = 0;
-        for(long i =k; i <n; i++){
-            if(i*i ==n)
-                return (i+1)*(i+1);
-            else
-                answer = -1;
+        public long solution(long n) {
+            long answer = -1;
+            if(Math.pow((int)Math.sqrt(n),2) == n)
+                return (long)Math.pow((Math.sqrt(n)+1),2);
+            return answer;
         }
-        return answer;
     }
-    public long solution(long n) {
-        long answer =0;
-        if( n<100000000)
-            answer = check(n,0);
-        else if(n>=100000000)
-            answer = check(n,10000);
-        else if (n>=1000000000000L)
-            answer = check(n,1000000);
 
-
-        return answer;
-    }
-}
 
