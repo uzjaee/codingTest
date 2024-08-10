c3b74a40972078df6929e5be00f6b0a2d7c78b84
uzjaee <kyj960802@gmail.com>
2024-08-10 10:19:40 +0900

    나머지가 1이 되는 수 찾기

--- programmers/pr_0005.java
+++ programmers/pr_0005.java
@@ -1,2 +1,12 @@
-package programmers;public class pr_0005 {
+package programmers;
+
+public class pr_0005 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i =1; i<n;i++){
+            if(n%i==1)
+                return i;
+        }
+        return answer;
+    }
 }
