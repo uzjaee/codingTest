b6cf4cbde2cef45b4326f95836fedbdac199bb5d
uzjaee <kyj960802@gmail.com>
2024-08-10 10:20:02 +0900

    하샤드 수

--- programmers/pr_0011.java
+++ programmers/pr_0011.java
@@ -1,2 +1,19 @@
-package programmers;public class pr_0011 {
+package programmers;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class pr_0011 {
+    public boolean solution(int x){
+        boolean answer = false;
+        // 숫자를 문자배열로
+        String []st = String.valueOf(x).split("");
+        // 문자배열을 숫자배열로
+        int  sum = Arrays.stream(st).mapToInt(Integer::parseInt).sum();
+        // 각 합을 더한 수를 x로 나누기
+        if(sum%x == 0)
+            return answer;
+        return answer;
+
+    }
 }
