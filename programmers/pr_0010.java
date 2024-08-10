f9e32918e0a8d4cf2a4899049b5c4824dd266a7a
uzjaee <kyj960802@gmail.com>
2024-08-10 10:19:52 +0900

    정수 내림차순 정렬

--- programmers/pr_0010.java
+++ programmers/pr_0010.java
@@ -1,2 +1,19 @@
-package programmers;public class pr_0010 {
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class pr_0010 {
+    public long solution(long n) {
+        long answer = 0;
+        // long > array(string)
+        String [] digit = Long.toString(n).split("");
+        // array.sort
+        Arrays.sort(digit, Collections.reverseOrder());
+        // array> String
+        answer = Long.parseLong(String.join("", digit));
+
+        return answer;
+    }
 }
