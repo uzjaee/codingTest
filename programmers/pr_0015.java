99cb32b34ff02ca6bf5087f4e67b6a8b2a6a4569
uzjaee <kyj960802@gmail.com>
2024-08-13 11:31:21 +0900

    나누어 떨어지는 숫자 배열 (스트림 사용)

--- programmers/pr_0015.java
+++ programmers/pr_0015.java
@@ -1,21 +1,15 @@
 package programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class pr_0015 {
     public int[] solution(int[] arr, int divisor) {
-        int[] answer = {-1};
-        List<Integer> li = new ArrayList<>();
-        for(int i :arr){
-            if(i%divisor == 0)
-                li.add(i);
-        }
-        if(li.size()==0)
-            return answer;
-        li.sort(Comparator.naturalOrder());
-        answer = li.stream().mapToInt(Integer::intValue).toArray();
+        int [] answer = Arrays.stream(arr).filter(i -> i % divisor == 0).toArray();
+        if(answer.length == 0) return new int[]{-1};
+        Arrays.sort(answer);
         return answer;
     }
 }
