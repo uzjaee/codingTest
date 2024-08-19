5a0e21398f8039d0215fc3d038a84d596aa164f5
uzjaee <kyj960802@gmail.com>
2024-08-19 09:32:44 +0900

    내적

---
+++ programmers/pr_0022.java
@@ -0,0 +1,13 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class pr_0022 {
+    public int solution(int[] a, int[] b) {
+        int answer =0;
+        for(int i=0;i<a.length;i++){
+            answer +=a[i]*b[i];
+        }
+        return answer;
+    }
+}
